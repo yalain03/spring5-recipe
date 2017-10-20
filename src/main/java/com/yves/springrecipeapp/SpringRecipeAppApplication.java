b734ca50b6a2d3d0f53b454f9703ccd46be0206e
yalain03 <yalain9@gmail.com>
2017-10-20 16:22:36 -0500

    Initial commit

---
+++ src/main/java/com/yves/springrecipeapp/SpringRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package com.yves.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeAppApplication.class, args);
+	}
+}
